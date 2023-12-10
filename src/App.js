758f5bbd81d45cdd4690895a63c5407731290402
nfischer1980 <89476786+nfischer1980@users.noreply.github.com>
2023-12-10 13:06:32 -0500

    Update App.js
    remove usestate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "./actions/shared";
